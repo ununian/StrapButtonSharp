0b788908a76e14ea96e465e7d7ff8af6a8c0c40e
秋声赋 <unhappy224@163.com>
2015-01-23 12:25:27 +0800

    新建

---
+++ StrapButtonSample/RootViewController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Drawing;
+
+using Foundation;
+using StrapButtonSharp;
+using UIKit;
+
+namespace StrapButtonSample
+{
+    public partial class RootViewController : UIViewController
+    {
+        public RootViewController(IntPtr handle)
+            : base(handle)
+        {
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            // Releases the view if it doesn't have a superview.
+            base.DidReceiveMemoryWarning();
+
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        #region View lifecycle
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            this.Btn1.DefaultStyle();
+            this.Btn1.SetTitle("Default", UIControlState.Normal);
+            Btn1.AddAwesomeIcon(AwesomeIcon.Bookmark, true);
+
+            this.Btn2.PrimaryStyle();
+            this.Btn2.SetTitle("Primary", UIControlState.Normal);
+            Btn2.AddAwesomeIcon(AwesomeIcon.Check, true);
+
+            this.Btn3.SuccessStyle();
+            this.Btn3.SetTitle("Success", UIControlState.Normal);
+            Btn3.AddAwesomeIcon(AwesomeIcon.DownloadAlt, false);
+
+            this.Btn4.InfoStyle();
+            this.Btn4.SetTitle("Info", UIControlState.Normal);
+
+
+            this.Btn5.WarningStyle();
+            this.Btn5.SetTitle("Warning", UIControlState.Normal);
+            Btn5.AddAwesomeIcon(AwesomeIcon.Cloud, true);
+
+            this.Btn6.DangerStyle();
+            this.Btn6.SetTitle("Danger", UIControlState.Normal);
+            Btn6.AddAwesomeIcon(AwesomeIcon.Star, false);
+
+            this.Btn7.SuccessStyle();
+            this.Btn7.SetTitle(" ", UIControlState.Normal);
+            Btn7.AddAwesomeIcon(AwesomeIcon.Calendar, true);
+
+            this.Btn8.SuccessStyle();
+            this.Btn8.SetTitle(" ", UIControlState.Normal);
+            Btn8.AddAwesomeIcon(AwesomeIcon.Dashboard, false);
+
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+        }
+
+        public override void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
+        }
+
+        public override void ViewWillDisappear(bool animated)
+        {
+            base.ViewWillDisappear(animated);
+        }
+
+        public override void ViewDidDisappear(bool animated)
+        {
+            base.ViewDidDisappear(animated);
+        }
+
+        #endregion
+    }
+}
---
+++ StrapButtonSample/RootViewController.designer.cs
@@ -0,0 +1,85 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace StrapButtonSample
+{
+	[Register ("RootViewController")]
+	partial class RootViewController
+	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton Btn1 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton Btn2 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton Btn3 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton Btn4 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton Btn5 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton Btn6 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton Btn7 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton Btn8 { get; set; }
+
+		void ReleaseDesignerOutlets ()
+		{
+			if (Btn1 != null) {
+				Btn1.Dispose ();
+				Btn1 = null;
+			}
+			if (Btn2 != null) {
+				Btn2.Dispose ();
+				Btn2 = null;
+			}
+			if (Btn3 != null) {
+				Btn3.Dispose ();
+				Btn3 = null;
+			}
+			if (Btn4 != null) {
+				Btn4.Dispose ();
+				Btn4 = null;
+			}
+			if (Btn5 != null) {
+				Btn5.Dispose ();
+				Btn5 = null;
+			}
+			if (Btn6 != null) {
+				Btn6.Dispose ();
+				Btn6 = null;
+			}
+			if (Btn7 != null) {
+				Btn7.Dispose ();
+				Btn7 = null;
+			}
+			if (Btn8 != null) {
+				Btn8.Dispose ();
+				Btn8 = null;
+			}
+		}
+	}
+}
